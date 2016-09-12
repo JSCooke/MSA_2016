fc40c55149236bcb967a8c80b58b97b52e29b4a5
JSCooke <jaydencooke@outlook.com>
2016-09-12 14:16:59 +1200

    Added colour change functionality for fonts

--- script.js
+++ script.js
@@ -19,7 +19,21 @@ function updateHex(type) {
     var r = parseInt(document.getElementById(type + "Red").value);
     var g = parseInt(document.getElementById(type + "Green").value);
     var b = parseInt(document.getElementById(type + "Blue").value);
-    document.getElementById(type + "Hex").innerHTML = "Hex: " + rgbToHex(r, g, b);
+    var colour = rgbToHex(r, g, b);
+    document.getElementById(type + "Hex").innerHTML = "Hex: " + colour;
+    if (type == "font") {
+        var fontElements;
+        for (var j = 1; j <= 6; j++) {
+            fontElements = document.getElementsByTagName("h" + j.toString());
+            for (var i = 0; i < fontElements.length; i++) {
+                fontElements[i].style.color = colour;
+            }
+        }
+        fontElements = document.getElementsByTagName("p");
+        for (var i = 0; i < fontElements.length; i++) {
+            fontElements[i].style.color = colour;
+        }
+    }
     localStorage.setItem("r", r.toString());
     localStorage.setItem("g", g.toString());
     localStorage.setItem("b", b.toString());
--- script.ts
+++ script.ts
@@ -21,8 +21,23 @@ function updateHex(type:string){
     let g: number = parseInt((<HTMLInputElement>document.getElementById(type+"Green")).value)
     let b: number = parseInt((<HTMLInputElement>document.getElementById(type+"Blue")).value)
 
-    document.getElementById(type+"Hex").innerHTML = "Hex: "+rgbToHex(r,g,b)
+    let colour: string = rgbToHex(r,g,b)
+    document.getElementById(type+"Hex").innerHTML = "Hex: "+colour
     
+    if (type == "font"){
+        var fontElements
+        for (var j = 1; j<=6; j++){
+            fontElements = document.getElementsByTagName("h"+j.toString())
+            for(var i = 0; i < fontElements.length; i++) {
+                fontElements[i].style.color = colour
+            }
+        }
+        fontElements = document.getElementsByTagName("p")
+        for(var i = 0; i < fontElements.length; i++) {
+            fontElements[i].style.color = colour
+        }
+    }
+
     localStorage.setItem("r",r.toString())
     localStorage.setItem("g",g.toString())
     localStorage.setItem("b",b.toString())
